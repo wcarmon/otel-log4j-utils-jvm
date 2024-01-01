cc0d821ca879f0f21c5bb33198ae0f0ada3785b9
Wil Carmon <java@wcarmon.com>
2024-01-01 11:04:19 -0500

    cleanup

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+/** module decl for otel */
+module io.github.wcarmon.otel.log4j {
+    exports io.github.wcarmon.otel.log4j;
+
+    requires io.opentelemetry.api;
+    requires io.opentelemetry.sdk.trace;
+    requires org.apache.logging.log4j.core;
+    requires org.apache.logging.log4j;
+    requires org.jetbrains.annotations;
+}
